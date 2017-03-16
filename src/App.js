ace957f819d615c9b761999c496d80048ed58512
Austin Blanton <imaus10@gmail.com>
2017-03-16 09:29:44 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GrainStorm extends Component {
+  render() {
+    return <h1>Hello, world!</h1>
+  }
+}
+
+export default GrainStorm;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GrainStorm from './App';
+import './index.css';
+
+ReactDOM.render(
+  <GrainStorm />,
+  document.getElementById('root')
+);
