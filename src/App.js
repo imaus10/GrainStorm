8ced2fcadaa8d0b9909516a06dbefc4ded58ba53
Austin Blanton <imaus10@gmail.com>
2017-03-22 11:54:00 -0400

    Change grain length

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import numeric from 'numeric';
 import './App.css';
+import numeric from 'numeric';
+import { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class GrainCloud extends Component {
   constructor(props) {
     super(props);
     this.state = { audioCtx: props.audioCtx
                  , audioData: props.audioData
+                 , grainStart: 0
+                 , grainEnd: 1
                  };
   }
   componentDidMount() {
@@ -14,7 +18,7 @@ class GrainCloud extends Component {
     const audio = this.state.audioData;
 
     // convert to mono
-    // console.log('converting...');
+    // console.log('converting to mono...');
     const nchan = audio.numberOfChannels;
     let canvasdata = audio.getChannelData(0);
     for (let ch=1; ch<nchan; ch++) {
@@ -28,47 +32,68 @@ class GrainCloud extends Component {
     canvasdata = numeric.sub(this.canvas.height, canvasdata);
     // console.log('converted.');
 
-    // draw
-    // TODO: for big files, step thru samples in chunks (avg chunks)
-    // console.log('drawing visualization...');
-    const stepsize = this.canvas.width/canvasdata.length;
-    for (let i=0; i<canvasdata.length-1; i++) {
+    // step thru the sample in chunks
+    const stepsize = Math.floor(canvasdata.length/this.canvas.width);
+    // draw a line for each pixel in canvas
+    // between the highest and lowest sample value in the chunk
+    for (let i=0; i<this.canvas.width; i++) {
       canvasCtx.beginPath();
-      canvasCtx.moveTo(stepsize*i, canvasdata[i]);
-      canvasCtx.lineTo(stepsize*(i+1), canvasdata[i+1]);
+      const chunk = canvasdata.slice(i*stepsize,(i+1)*stepsize);
+      canvasCtx.moveTo(i, Math.min.apply(null, chunk));
+      canvasCtx.lineTo(i, Math.max.apply(null, chunk));
       canvasCtx.stroke();
     }
     // console.log('drawn.');
+    this.audioImage = canvasCtx.getImageData(0,0,this.canvas.width,this.canvas.height);
+  }
+  componentDidUpdate() {
+    // draw opaque gray rectangle over non-playing audio
+    const canvasCtx = this.canvas.getContext('2d');
+    canvasCtx.putImageData(this.audioImage, 0, 0);
+    canvasCtx.fillStyle = 'rgba(233,233,233,0.8)';
+    const w = this.canvas.width;
+    const h = this.canvas.height;
+    canvasCtx.fillRect(0, 0, w*this.state.grainStart, h);
+    canvasCtx.fillRect(w*this.state.grainEnd, 0, w*(1-this.state.grainEnd), h);
   }
   render() {
+    const style = { width: 300 };
     return (
       <div>
         <canvas ref={c => {this.canvas = c}}></canvas>
+        <div style={style}>
+          <Range allowCross={false} defaultValue={[0,100]} onChange={value => this.changeGrain(value)} />
+        </div>
         <button type="button" onClick={() => this.playGrain()}>play</button>
       </div>
     );
+  }
+  changeGrain(value) {
+    this.setState({ grainStart: value[0]/100, grainEnd: value[1]/100 });
   }
   playGrain() {
     const audioCtx = this.state.audioCtx;
     const soundSource = audioCtx.createBufferSource();
     soundSource.buffer = this.state.audioData;
     soundSource.connect(audioCtx.destination);
-    const startTime = audioCtx.currentTime;
-    soundSource.start();
+    const playTime = audioCtx.currentTime;
+    const startTime = this.state.grainStart*soundSource.buffer.duration;
+    const endTime = this.state.grainEnd*soundSource.buffer.duration;
+    soundSource.start(0, startTime, endTime-startTime);
 
     let animation;
     const canvasCtx = this.canvas.getContext('2d');
-    const amplitudeImage = canvasCtx.getImageData(0,0,this.canvas.width,this.canvas.height);
+    const currCanvas = canvasCtx.getImageData(0,0,this.canvas.width,this.canvas.height);
     soundSource.onended = () => {
       window.cancelAnimationFrame(animation);
-      canvasCtx.putImageData(amplitudeImage, 0, 0);
+      canvasCtx.putImageData(currCanvas, 0, 0);
     };
     const drawPos = () => {
       animation = window.requestAnimationFrame(drawPos);
       const now = audioCtx.currentTime;
-      const pos = Math.floor(soundSource.buffer.sampleRate * (now - startTime));
+      const pos = Math.floor(soundSource.buffer.sampleRate * (now - playTime + startTime));
       const stepsize = this.canvas.width/soundSource.buffer.getChannelData(0).length
-      canvasCtx.putImageData(amplitudeImage, 0, 0);
+      canvasCtx.putImageData(currCanvas, 0, 0);
       canvasCtx.strokeStyle = 'red';
       canvasCtx.beginPath();
       canvasCtx.moveTo(stepsize*pos, 0);
