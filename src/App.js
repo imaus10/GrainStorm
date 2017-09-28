be7011424d1bc413eb0ff18eb83dbabd7651055c
Austin Blanton <imaus10@gmail.com>
2017-09-28 12:21:17 -0400

    get rid of key warning

--- src/App.js
+++ src/App.js
@@ -236,93 +236,72 @@ class GrainStorm extends Component {
   }
 
   static walkthruHelp = [
-  // 0
-    [ <div>
+    [ <div key="0">
         <p>Granular synthesis is an electronic music technique that creates tiny "grains" of sound.</p>
         <p>Though each individual sound particle is miniscule, when many grains overlap, they create a texture known as a cloud.</p>
         <p>Generate a grain cloud by clicking the glowing button above.</p>
       </div>, false ]
-  // 1
-  , [ <p>Start the cloud by pressing the play button.</p>, false ]
-  // 2
-  , [ <div>
+  , [ <div key="1"><p>Start the cloud by pressing the play button.</p></div>, false ]
+  , [ <div key="2">
         <p>The playback sounds stuttery because the red playhead is taking a 30 millisecond grain sample every 100 milliseconds - there are gaps between grains.</p>
         <p>Grain density is how close together grains are packed. That is, when you increase the density, more grains are created, and grains overlap more.</p>
         <p>Move the slider to see what this sounds like.</p>
       </div>, true ]
-  // 3
-  , [ <div>
+  , [ <div key="3">
         <p>Increasing grain duration also increases overlap, for a kind of chorus effect that multiplies the volume.</p>
         <p>Try a high density and a low duration and see what happens.</p>
       </div>, true ]
-  // 4
-  , [ <div>
+  , [ <div key="4">
         <p>Use the slider below the playhead viewer to make grains from a specific part of the sample.</p>
         <p>You can get interesting repetitive sounds by narrowing in on a small section.</p>
       </div>, true ]
-  // 5
-  , [ <div>
+  , [ <div key="5">
         <p>Rather than always sample exactly where the playhead is, you can opt to take grains from a random position nearby the playhead. This scattered sound is often what people think of when they think about granular synthesis.</p>
         <p>Try it out by increasing the randomness parameter. As randomness increases, the grains will be sampled from a larger area around the playhead. If you slide it all the way to the right, each sample will come from a completely random position.</p>
       </div>, true ]
-  // 6
-  , [ <div>
+  , [ <div key="6">
         <p>Change the speed of the playhead.</p>
         <p>Move the slider exactly to the middle and the playhead stops moving, and makes the same grain over and over.</p>
         <p>Move to the left of middle, and the playhead direction reverses. Each grain is still played forward, but the sample position is moving backward.</p>
         <p>The farther the slider is from the middle, the faster the playhead goes.</p>
       </div>, true ]
-  // 7
-  , [ <p>Try changing the pitch of the sample.</p>, true ]
-  // 8
-  , [ <div>
+  , [ <div key="7"><p>Try changing the pitch of the sample.</p></div>, true ]
+  , [ <div key="8">
         <p>Each grain has an envelope that controls how quickly the sample reaches full volume (attack) and how quickly it fades out (decay). Below the envelope screen is a slider with two knobs that control the attack and decay.</p>
         <p>Without the envelope, grains sound clicky. Verify this by sliding the attack knob all the way left and the decay knob all the way right.</p>
       </div>, true ]
-  // 9
-  , [ <p>Now try different envelopes and see how the sound changes.</p>, true ]
-  // 10
-  , [ <div>
+  , [ <div key="9"><p>Now try different envelopes and see how the sound changes.</p></div>, true ]
+  , [ <div key="10">
         <p>Now for the fun part! See the PARAMETER CTRL panel that appeared below? You can use this to automatically move knobs.</p>
         <p>First, click the blue "show" button to the right of the PARAMETER CTRL label.</p>
       </div>, false ]
-  // 11
-  , [ <p>Now click any of the blue sliders to start controlling it.</p>, false ]
-  // 12
-  , [ <div>
+  , [ <div key="11"><p>Now click any of the blue sliders to start controlling it.</p></div>, false ]
+  , [ <div key="12">
         <p>You are now using a low frequency oscillator (LFO) on your parameter!</p>
         <p>The parameter value alternates between the low knob and the high knob. Drag these around and see what happens.</p>
       </div>, true ]
-  // 13
-  , [ <div>
+  , [ <div key="13">
         <p>The PARAMETER CTRL panel has additional parameters for whatever control function you select.</p>
         <p>For example, LFO has a period, which controls how quickly the parameter alternates between the low and high values. Try changing the period til you find a value you like.</p>
       </div>, true ]
-  // 14
-  , [ <p>Now that you've learned the basics of the LFO, try out some other functions. Select from the "Control function" dropdown in the PARAMETER CTRL panel.</p>, true ]
-  // 15
-  , [ <div>
+  , [ <div key="14"><p>Now that you've learned the basics of the LFO, try out some other functions. Select from the "Control function" dropdown in the PARAMETER CTRL panel.</p></div>, true ]
+  , [ <div key="15">
         <p>If you want to remove automatic control, click the middle knob, and the slider will return to the blue color.</p>
         <p>You may now also click on other blue parameters to control those as well.</p>
         <p>Once you're done setting controls, click the blue "hide" button next to PARAMETER CTRL.</p>
       </div>, false ]
-  // 16
-  , [ <div>
+  , [ <div key="16">
         <p>Purple sliders will remain controlled, but now you can set green sliders manually again.</p>
         <p>Just a couple more things before you can explore on your own!</p>
       </div>, true ]
-  // 17
-  , [ <p>There's a new button above! It will add another grain source, this time using a pure sound wave instead of a sampled sound. Click it!</p>, false ]
-  // 18
-  , [ <p>Wave form grains have many of the same parameters, but frequency and wave type are unique to wave forms.</p>, true ]
-  // 19
-  , [ <div>
+  , [ <div key="17"><p>There's a new button above! It will add another grain source, this time using a pure sound wave instead of a sampled sound. Click it!</p></div>, false ]
+  , [ <div key="18"><p>Wave form grains have many of the same parameters, but frequency and wave type are unique to wave forms.</p></div>, true ]
+  , [ <div key="19">
         <p>Two more things -- </p>
         <p>You can now add as many grain sources as you want! Check out the glowing buttons above.</p>
         <p>If you want to get rid of a grain source, click the X at the top right, next to the volume.</p>
       </div>, true ]
-  // 20
-  , [ <div>
+  , [ <div key="20">
         <p>Congratulations! You've made it through the tutorial! Now go make some weird noises.</p>
         <p>Click the "skip tutorial" button to get this help screen out of your way.</p>
       </div>, false ]
